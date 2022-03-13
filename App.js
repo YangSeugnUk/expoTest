f8271957cef3d6358d04c7232cfbf8dcc8cb0c9d
Yang <9488372@hanmail.net>
2022-03-13 12:08:04 +0900

    axios

--- App.js
+++ App.js
@@ -1,60 +1,55 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button} from "react-native";
+import {View, Text, ScrollView, Image} from 'react-native';
+import axios from "axios";
 
 const App = () => {
-    const [datas, setDatas] = useState([]);
-    const users = [
-        {
-            name : "seunguk",
-            age :  35,
-            gender : "male",
-            address : "hh",
-        },
-        {
-            name : "seunguk",
-            age :  35,
-            gender : "male",
-            address : "hh",
-        },
-        {
-            name : "seunguk",
-            age :  35,
-            gender : "male",
-            address : "hh",
-        },
 
+    //1. 상태선언
+    const [movies, setMovies] = useState([])
 
-    ]
-    const getData = () => {
-        setDatas(users);
+    //3. 네트워킹 및 파싱 함수 선언
+    const getMovies = async () =>{
+        try{
+
+            const {data, status} = await axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US&page=1")
+            if ( status === 200 )
+            {
+                setMovies(data.results);
+            }
+
+        }catch (err){
+            alert(err.response.data.message);
+        }
 
     }
-    const clearData = () => {
-        setDatas([]);
-    }
 
-    // 1순위로 무조건 실행되는 함수.
+    //2. Effect 선언
     useEffect(() => {
-        getData();
-    }, [])
+        getMovies();
+    },[])
 
     return (
-        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-            <Text>
-                {datas.length}
-            </Text>
-            <Button
-                title="data불러오기"
-                onPress={() => getData()}
-            />
-            {datas.map(data =>(
-                <Text>{data.name}</Text>
-            ))}
-            <Button
-                title="data비우기"
-                onPress={() => clearData()}
-            />
-        </View>
+        <ScrollView >
+            <View style={{alignItems:"center" }}>
+                {movies.map(movie => (
+                    <>
+                        <Text style={{color:"purple"  }}>
+                            {movie.original_title}
+                        </Text>
+                        <Text style={{marginBottom:40}}>
+                            {movie.overview}
+                        </Text>
+                        <Image
+                            style={{width:300, height:300}}
+                            source={{
+                                uri : "https://image.tmdb.org/t/p/w500"+movie.poster_path
+                            }}
+                        />
+                    </>
+
+                ))}
+            </View>
+        </ScrollView>
     );
 };
 
