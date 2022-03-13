16f9158d0862d8bf7ae93de8da167a15048ad916
Yang <9488372@hanmail.net>
2022-03-13 11:00:27 +0900

    useState get set

--- App.js
+++ App.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import {View, Text} from "react-native";
+import React, {useState} from 'react';
+import {View, Text, Button} from "react-native";
 
-// 함수
 const App = () => {
-
-    // 함수, 상수, 변수, 상태
-    // const yangseunguk = {
-    //     name : "seunguk",
-    //     age :  35,
-    //     gender : "male",
-    //     address : "hh",
-    //
-    // }
-    const datas = [
+    const [datas, setDatas] = useState([]);
+    const users = [
         {
             name : "seunguk",
             age :  35,
@@ -34,28 +25,32 @@ const App = () => {
 
 
     ]
+    const getData = () => {
+        setDatas(users);
 
+    }
+    const clearData = () => {
+        setDatas([]);
+    }
 
-    // 실질적으로 화면에 보여주는 부분
     return (
-        <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
-            {datas.map(data => (
-                <Text>
-                    {data.name}
-                </Text>
+        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+            <Text>
+                {datas.length}
+            </Text>
+            <Button
+                title="data불러오기"
+                onPress={() => getData()}
+            />
+            {datas.map(data =>(
+                <Text>{data.name}</Text>
             ))}
-
+            <Button
+                title="data비우기"
+                onPress={() => clearData()}
+            />
         </View>
     );
 };
 
 export default App;
-
-//  styleSheet 선언
-
-// 코딩기본법칙
-// 1. 위에서 아래 순서로 빌드를 한다.
-// 2. = 을 기준으로 오른쪽에서 왼쪽으로 치환한다.
-// 3. . 하위메소드 호출
-// 4. {} 직접호출
-// 5. () 함수들의 모음
