d715baded4a633ea394133464503e1646520c816
Yang <9488372@hanmail.net>
2022-03-13 10:21:12 +0900

    component test

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, Image, Button, TextInput, StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    return (
+        <View>
+          <Text>
+            hi
+          </Text>
+          <Image
+            source={{
+              uri: "https://thumb.mt.co.kr/06/2020/07/2020070608543287598_1.jpg/dims/optimize/"
+            }}
+            style={{ width:200, height:200}}
+          />
+          <Button
+            title="Press me"
+            onPress={() => alert("hi")}
+          />
+          <TextInput
+              style={styles.input}
+
+            placeholder="hi"
+            keyboardType="text"
+          />
+          
+        </View>
+    );
+};
+
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  input:{
+    height:40,
+    margin:12,
+    borderWidth:1,
+    padding:10,
+  }
+})
